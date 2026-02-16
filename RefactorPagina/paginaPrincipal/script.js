9e03a55cc98d8f9096b767bd794d6a93707d1fa7
Joseph Olarte <josephsantiago2345@gmail.com>
2026-02-16 11:38:06 -0500

    Añadiendo la logica para expandir las imagenes del .feature-image

--- RefactorPagina/paginaPrincipal/script.js
+++ RefactorPagina/paginaPrincipal/script.js
@@ -1,3 +1,4 @@
+// Animacion al realizar scroll
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -11,6 +12,7 @@ const observer = new IntersectionObserver((entries) => {
 const elementosAanimar = document.querySelectorAll('.revelar, .revelar-izq');
 elementosAanimar.forEach((el) => observer.observe(el));
 
+// Header scroll
 window.addEventListener('scroll', function() {
     const header = document.querySelector('.navbar');
     const hero = document.querySelector('.hero');
@@ -20,4 +22,27 @@ window.addEventListener('scroll', function() {
     } else {
         header.classList.remove('header-active');
     }
+});
+
+// Expandir imagenes
+const lightbox = document.createElement('div');
+lightbox.className = 'lightbox';
+document.body.appendChild(lightbox);
+
+const imgContainer = document.querySelectorAll('.feature-image img');
+
+imgContainer.forEach(image => {
+    image.addEventListener('click', () => {
+        lightbox.classList.add('active');
+        const img = document.createElement('img');
+        img.src = image.src;
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img);
+    });
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.classList.remove('active');
 });
