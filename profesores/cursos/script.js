291cc501aee557cd5dd6680d7cf6278d2a3b2d59
Joseph <josephsantiago2345@gmail.com>
2026-06-04 11:22:44 -0500

    Separando el js del html

--- profesores/cursos/script.js
+++ profesores/cursos/script.js
@@ -1,13 +1,17 @@
-function abrirModal(idModal) {
-    const modal = document.getElementById(idModal);
-    modal.showModal();
-}
+function abrirModal(id) {
+        document.getElementById(id).showModal();
+    }
 
-function cerrarModal(idModal) {
-    const modal = document.getElementById(idModal);
-    modal.close();
-}
+    function cerrarModal(id) {
+        document.getElementById(id).close();
+    }
 
-function irAPanel(curso, materia) {
-    alert(`Entrando a: ${materia} de ${curso}`);
-}
+    function irAPanel(curso, materia) {
+        console.log(`Navegar al panel de ${materia} — ${curso}`);
+    }
+
+    document.querySelectorAll('.modal').forEach(modal => {
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) modal.close();
+        });
+    });
